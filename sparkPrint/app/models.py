01ef6b2e19185b8e9cfd89eafc74b34b51d1a4ef
Matt Hova <matt@igroupcreative.com>
2014-09-24 00:00:19 -0700

    sql working

--- sparkPrint/app/models.py
+++ sparkPrint/app/models.py
@@ -9,3 +9,11 @@ class Printer(db.Model):
 
     def __repr__(self):
         return '<Printer %r>' % (self.model_name)
+        
+class PrintJob(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    timestamp = db.Column(db.DateTime)
+    printer_id = db.Column(db.Integer, db.ForeignKey('printer.id'))
+    
+    def __repr__(self):
+        return '<PrintJob %r>' % (self.id)
